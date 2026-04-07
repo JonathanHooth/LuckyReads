cd101bcec5b72ea3bb6cdcfb26c630585c0feb09
Jonathan Hooth <portalmaster9999@gmail.com>
2026-04-07 01:43:10 -0400

    Undid accidental bio field deletion

--- app/apps/books/models.py
+++ app/apps/books/models.py
@@ -8,7 +8,7 @@ class Author(models.Model):
         db_index=True
     )
     name = models.CharField(max_length=128)
-    4
+    bio = models.TextField(blank=True, default='')
     photo_url = models.URLField(blank=True, default='')
 
     class Meta:
